d245797f834fc035a8b887078c76eef8b317637e
Carlos Carneiro <carlos.carneiro@affinipay.com>
2024-08-22 16:53:15 -0300

    Finished install script

--- install.sh
+++ install.sh
@@ -4,5 +4,17 @@ echo "Pulling latest version from github"
 git pull
 echo "Success"
 
-echo "Creating backup of current version, if any"
+echo "Checking if a tmux config file already exists"
+if [ ! -f $HOME/.tmux.conf ]; then
+	echo "No config file found, skipping backup";
+else
+	echo "Creating backup of current file"
+	mv $HOME/.tmux.conf $HOME/.tmux.conf.$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+	echo "Success"
+fi
+
+echo "Copying latest config file to home directory"
+cp tmux.conf $HOME/.tmux.conf
+
+echo "tmux config file successfully installed"
 
