8aa127be9f6b4152bba09f0192aebe563ebb0091
Carlos Carneiro <carlos.carneiro@affinipay.com>
2024-08-22 16:35:23 -0300

    First commit

---
+++ install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting installation of my latest tmux config"
+echo "Pulling latest version from github"
+git pull
+echo "Success"
+
+echo "Creating backup of current version, if any"
+
